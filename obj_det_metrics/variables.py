31310d96a56169b4e0c9732e0042334cf780c3d7
yxlee245 <the.force.of.life@gmail.com>
2021-12-12 01:51:14 +0800

    Modify bounding box class

--- obj_det_metrics/variables.py
+++ obj_det_metrics/variables.py
@@ -12,7 +12,26 @@ class BoundingBox:
         conf_score: float = 1.0,
         matched: bool = False,
     ):
-        self.coordinates = coordinates
-        self.class_name = class_name
-        self.conf_score = conf_score
-        self.matched = matched
+        self._coordinates = coordinates
+        self._class_name = class_name
+        self._conf_score = conf_score
+        self._matched = matched
+
+    def set_matched(self, matched: bool):
+        self._matched = matched
+
+    @property
+    def coordinates(self) -> Coordinates:
+        return self._coordinates
+
+    @property
+    def class_name(self) -> ClassName:
+        return self._class_name
+
+    @property
+    def conf_score(self) -> float:
+        return self._conf_score
+
+    @property
+    def matched(self) -> bool:
+        return self._matched
