b1b368886e7d08fa350ad49d46afb4a3e8f3061e
yxlee245 <the.force.of.life@gmail.com>
2021-12-12 02:38:41 +0800

    Modify type annotations and add docstrings

--- obj_det_metrics/ap_map.py
+++ obj_det_metrics/ap_map.py
@@ -2,7 +2,7 @@
 Adapted from https://github.com/LeMuecke/mapcalc
 """
 
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple
 
 import numpy as np
 
@@ -15,36 +15,43 @@ from obj_det_metrics.utils import (
 from obj_det_metrics.variables import ClassName
 
 
-def _voc_ap(rec, prec):
-    """
-     Calculate the AP given the recall and precision array
-        1st) We compute a version of the measured precision/recall curve with
-             precision monotonically decreasing
-        2nd) We compute the AP as the area under this curve by numerical integration.
+def _voc_ap(rec: List[float], prec: List[float]) -> Tuple[float, List[float], List[float]]:
+    """Calculate the AP given the recall and precision array
+    1st) We compute a version of the measured precision/recall curve with
+        precision monotonically decreasing
+    2nd) We compute the AP as the area under this curve by numerical integration.
 
     --- Official matlab code VOC2012---
     mrec=[0 ; rec ; 1];
     mpre=[0 ; prec ; 0];
     for i=numel(mpre)-1:-1:1
-            mpre(i)=max(mpre(i),mpre(i+1));
+        mpre(i)=max(mpre(i),mpre(i+1));
     end
     i=find(mrec(2:end)~=mrec(1:end-1))+1;
     ap=sum((mrec(i)-mrec(i-1)).*mpre(i));
-    """
 
-    rec.insert(0, 0.0)  # insert 0.0 at begining of list
-    rec.append(1.0)  # insert 1.0 at end of list
+    Args:
+        rec (List[float]): List of recall values
+        prec (List[float]): List of precision values
+
+    Returns:
+        Tuple[float, List[float], List[float]]: Tuple containing average precision, mean recall and
+            mean precision values
+    """
+    # insert 0.0 at beginning and 1.0 at end of list
+    rec.insert(0, 0.0)
+    rec.append(1.0)
     mrec = rec[:]
-    prec.insert(0, 0.0)  # insert 0.0 at begining of list
-    prec.append(0.0)  # insert 0.0 at end of list
+
+    # insert 0.0 at beginning and end of list
+    prec.insert(0, 0.0)
+    prec.append(0.0)
     mpre = prec[:]
 
-    """
-     This part makes the precision monotonically decreasing
-        (goes from the end to the beginning)
-        matlab: for i=numel(mpre)-1:-1:1
-                    mpre(i)=max(mpre(i),mpre(i+1));
-    """
+    #  This part makes the precision monotonically decreasing
+    #     (goes from the end to the beginning)
+    #     matlab: for i=numel(mpre)-1:-1:1
+    #                 mpre(i)=max(mpre(i),mpre(i+1));
 
     # matlab indexes start in 1 but python in 0, so I have to do:
     #     range(start=(len(mpre) - 2), end=0, step=-1)
@@ -52,68 +59,66 @@ def _voc_ap(rec, prec):
     #     range(start=(len(mpre) - 2), end=-1, step=-1)
     for i in range(len(mpre) - 2, -1, -1):
         mpre[i] = max(mpre[i], mpre[i + 1])
-    """
-     This part creates a list of indexes where the recall changes
-        matlab: i=find(mrec(2:end)~=mrec(1:end-1))+1;
-    """
+
+    #  This part creates a list of indexes where the recall changes
+    #     matlab: i=find(mrec(2:end)~=mrec(1:end-1))+1;
     i_list = []
     for i in range(1, len(mrec)):
         if mrec[i] != mrec[i - 1]:
             i_list.append(i)  # if it was matlab would be i + 1
-    """
-     The Average Precision (AP) is the area under the curve
-        (numerical integration)
-        matlab: ap=sum((mrec(i)-mrec(i-1)).*mpre(i));
-    """
+
+    #  The Average Precision (AP) is the area under the curve
+    #     (numerical integration)
+    #     matlab: ap=sum((mrec(i)-mrec(i-1)).*mpre(i));
     ap = 0.0
     for i in i_list:
         ap += (mrec[i] - mrec[i - 1]) * mpre[i]
     return ap, mrec, mpre
 
 
-def _check_dicts_for_content_and_size(ground_truth_dict: dict, result_dict: dict):
-    """
-
-    Checks if the content and the size of the arrays adds up.
+def _check_dicts_for_content_and_size(ground_truth_dict: Dict[str, Any], result_dict: Dict[str, Any]):
+    """Checks if the content and the size of the arrays adds up.
     Raises and exception if not, does nothing if everything is ok.
 
-    :param ground_truth_dict: dict with {boxes:, labels:}
-    e.g.
-    {
-    'boxes':
-        [[60., 80., 66., 92.],
-         [59., 94., 68., 97.],
-         [70., 87., 81., 94.],
-         [8., 34., 10., 36.]],
+    Args:
+        ground_truth_dict (Dict[str, Any]): Dict with {boxes:, labels:}
+        e.g.
+        {
+        'boxes':
+            [[60., 80., 66., 92.],
+            [59., 94., 68., 97.],
+            [70., 87., 81., 94.],
+            [8., 34., 10., 36.]],
 
-    'labels':
-        [2, 2, 3, 4]}
-    :param result_dict: dict with {boxes:, labels:, scores:}
-    e.g.
-    {
-    'boxes':
-        [[57., 87., 66., 94.],
-         [58., 94., 68., 95.],
-         [70., 88., 81., 93.],
-         [10., 37., 17., 40.]],
+        'labels':
+            [2, 2, 3, 4]}
+        result_dict (Dict[str, Any]): Dict with {boxes:, labels:, scores:}
+        e.g.
+        {
+        'boxes':
+            [[57., 87., 66., 94.],
+                [58., 94., 68., 95.],
+                [70., 88., 81., 93.],
+                [10., 37., 17., 40.]],
 
-    'labels':
-        [2, 3, 3, 4],
+        'labels':
+            [2, 3, 3, 4],
 
-    'scores':
-        [0.99056727, 0.98965424, 0.93990153, 0.9157755]}
-    :return:
+        'scores':
+            [0.99056727, 0.98965424, 0.93990153, 0.9157755]}
+
+    Raises:
+        KeyError: Error raised if keys "boxes" or "labels" not found in `ground_truth_dict`
+        KeyError: Error raised if keys "boxes" or "labels" not found in `result_dict`
+        ValueError: Error raised if number of boxes and labels differ in `ground_truth_dict`
+        ValueError: Error raised if number of boxes and labels differ in `result_dict`
     """
-    if "boxes" not in ground_truth_dict.keys():
-        raise ValueError("ground_truth_dict expects the keys 'boxes' and 'labels'.")
-    if "labels" not in ground_truth_dict.keys():
-        raise ValueError("ground_truth_dict expects the keys 'boxes' and 'labels'.")
-    if "boxes" not in result_dict.keys():
-        raise ValueError("result_dict expects the keys 'boxes' and 'labels' and optionally 'scores'.")
-    if "labels" not in result_dict.keys():
-        raise ValueError("result_dict expects the keys 'boxes' and 'labels' and optionally 'scores'.")
+    if "boxes" not in ground_truth_dict or "labels" not in ground_truth_dict:
+        raise KeyError("ground_truth_dict expects the keys 'boxes' and 'labels'.")
+    if "boxes" not in result_dict or "labels" not in result_dict:
+        raise KeyError("result_dict expects the keys 'boxes' and 'labels' and optionally 'scores'.")
 
-    if "scores" not in result_dict.keys():
+    if "scores" not in result_dict:
         result_dict["scores"] = [1] * len(result_dict["boxes"])
 
     if len(ground_truth_dict["boxes"]) != len(ground_truth_dict["labels"]):
@@ -123,39 +128,42 @@ def _check_dicts_for_content_and_size(ground_truth_dict: dict, result_dict: dict
         raise ValueError("The number of boxes, labels and scores differ in the result_dict.")
 
 
-def calculate_ap_map(ground_truth_dict: dict, result_dict: dict, iou_threshold: float):
+def calculate_ap_map(
+    ground_truth_dict: Dict[str, Any], result_dict: Dict[str, Any], iou_threshold: float
+) -> Dict[str, Any]:
+    """Compute AP@[iou_threshold] for all classes and mAP@[iou_threshold]
+
+    Args:
+        ground_truth_dict (Dict[str, Any]): Dict with {boxes:, labels:}
+        e.g.
+        {
+        'boxes':
+            [[60., 80., 66., 92.],
+                [59., 94., 68., 97.],
+                [70., 87., 81., 94.],
+                [8., 34., 10., 36.]],
+
+        'labels':
+            [2, 2, 3, 4]}
+        result_dict (Dict[str, Any]): Dict with {boxes:, labels:, scores:}
+        e.g.
+        {
+        'boxes':
+            [[57., 87., 66., 94.],
+            [58., 94., 68., 95.],
+            [70., 88., 81., 93.],
+            [10., 37., 17., 40.]],
+
+        'labels':
+            [2, 3, 3, 4],
+
+        'scores':
+            [0.99056727, 0.98965424, 0.93990153, 0.9157755]}
+        iou_threshold (float): minimum iou for which the detection counts as true positive
+
+    Returns:
+        Dict[str, Any]: Dict containing APs for all classes and mAP
     """
-    mAP@[iou_threshold]
-
-    :param ground_truth_dict: dict with {boxes:, labels:}
-    e.g.
-    {
-    'boxes':
-        [[60., 80., 66., 92.],
-         [59., 94., 68., 97.],
-         [70., 87., 81., 94.],
-         [8., 34., 10., 36.]],
-
-    'labels':
-        [2, 2, 3, 4]}
-    :param result_dict: dict with {boxes:, labels:, scores:}
-    e.g.
-    {
-    'boxes':
-        [[57., 87., 66., 94.],
-         [58., 94., 68., 95.],
-         [70., 88., 81., 93.],
-         [10., 37., 17., 40.]],
-
-    'labels':
-        [2, 3, 3, 4],
-
-    'scores':
-        [0.99056727, 0.98965424, 0.93990153, 0.9157755]}
-    :param iou_threshold: minimum iou for which the detection counts as successful
-    :return: mean average precision (mAP)
-    """
-
     # checking if the variables have the correct keys
 
     _check_dicts_for_content_and_size(ground_truth_dict, result_dict)
@@ -164,7 +172,7 @@ def calculate_ap_map(ground_truth_dict: dict, result_dict: dict, iou_threshold: 
     unique, counts = np.unique(ground_truth_dict["labels"], return_counts=True)
     ground_truth_counter_per_class = dict(zip(unique, counts))
     count_true_positives: Dict[ClassName, int] = {}
-    sum_average_precision = 0
+    sum_average_precision = 0.0
 
     ground_truth_list = _generate_ground_truths(ground_truth_dict)
     outputs_dict: Dict[str, Any] = {
@@ -208,46 +216,47 @@ def calculate_ap_map(ground_truth_dict: dict, result_dict: dict, iou_threshold: 
 
 
 def calculate_ap_map_range(
-    ground_truth_dict: dict,
-    result_dict: dict,
+    ground_truth_dict: Dict[str, Any],
+    result_dict: Dict[str, Any],
     iou_begin: float,
     iou_end: float,
     iou_step: float,
-):
-    """
-    Gives mAP@[iou_begin:iou_end:iou_step], including iou_begin and iou_end.
+) -> Dict[str, Any]:
+    """Computes AP@[iou_begin:iou_end:iou_step] for all classes and mAP@[iou_begin:iou_end:iou_step],
+        including iou_begin and iou_end.
 
-    :param ground_truth_dict: dict with {boxes:, labels:}
-    e.g.
-    {
-    'boxes':
-        [[60., 80., 66., 92.],
-         [59., 94., 68., 97.],
-         [70., 87., 81., 94.],
-         [8., 34., 10., 36.]],
+    Args:
+        ground_truth_dict (Dict[str, Any]): Dict with {boxes:, labels:}
+        e.g.
+        {
+        'boxes':
+            [[60., 80., 66., 92.],
+            [59., 94., 68., 97.],
+            [70., 87., 81., 94.],
+            [8., 34., 10., 36.]],
 
-    'labels':
-        [2, 2, 3, 4]}
-    :param result_dict: dict with {boxes:, labels:, scores:}
-    e.g.
-    {
-    'boxes':
-        [[57., 87., 66., 94.],
-         [58., 94., 68., 95.],
-         [70., 88., 81., 93.],
-         [10., 37., 17., 40.]],
+        'labels':
+            [2, 2, 3, 4]}
+        result_dict (Dict[str, Any]): Dict with {boxes:, labels:, scores:}
+        e.g.
+        {
+        'boxes':
+            [[57., 87., 66., 94.],
+            [58., 94., 68., 95.],
+            [70., 88., 81., 93.],
+            [10., 37., 17., 40.]],
 
-    'labels':
-        [2, 3, 3, 4],
+        'labels':
+            [2, 3, 3, 4],
 
-    'scores':
-        [0.99056727, 0.98965424, 0.93990153, 0.9157755]}
-    :param iou_begin: first iou to evaluate
-    :param iou_end: last iou to evaluate (included!)
-    :param iou_step: step size
-    :param allow_cut_off: If true, there will be no exception if the number of predictions is not the same than
-    the number of ground truth values. Will cut off predictions with the least scores.
-    :return: mean average precision
+        'scores':
+            [0.99056727, 0.98965424, 0.93990153, 0.9157755]}
+        iou_begin (float): first iou threshold to use for evaluation
+        iou_end (float): last iou threshold to use for evaluation (included!)
+        iou_step (float): step size
+
+    Returns:
+        Dict[str, Any]: Dict containing APs and mAP averaged over all IOU threshold values
     """
 
     _check_dicts_for_content_and_size(ground_truth_dict, result_dict)
--- obj_det_metrics/utils.py
+++ obj_det_metrics/utils.py
@@ -19,6 +19,28 @@ def read_file_lines_to_list(filepath: str) -> List[str]:
 
 
 def _generate_detections_by_class(result_dict: Dict[str, Any], class_name: ClassName) -> List[BoundingBox]:
+    """Generate list of detection bounding box objects by class
+
+    Args:
+        result_dict (Dict[str, Any]): Dict with {boxes:, labels:, scores:}
+        e.g.
+        {
+        'boxes':
+            [[57., 87., 66., 94.],
+            [58., 94., 68., 95.],
+            [70., 88., 81., 93.],
+            [10., 37., 17., 40.]],
+
+        'labels':
+            [2, 3, 3, 4],
+
+        'scores':
+            [0.99056727, 0.98965424, 0.93990153, 0.9157755]}
+        class_name (ClassName): Class name of bounding boxes to be returned
+
+    Returns:
+        List[BoundingBox]: Bounding boxes for detections
+    """
     detections: List[BoundingBox] = []
     for idx in range(len(result_dict["labels"])):
         if result_dict["labels"][idx] == class_name:
@@ -33,6 +55,24 @@ def _generate_detections_by_class(result_dict: Dict[str, Any], class_name: Class
 
 
 def _generate_ground_truths(ground_truth_dict: Dict[str, Any]) -> List[BoundingBox]:
+    """Generate list of ground truth bounding boxes
+
+    Args:
+        ground_truth_dict (Dict[str, Any]): Dict with {boxes:, labels:}
+        e.g.
+        {
+        'boxes':
+            [[60., 80., 66., 92.],
+            [59., 94., 68., 97.],
+            [70., 87., 81., 94.],
+            [8., 34., 10., 36.]],
+
+        'labels':
+            [2, 2, 3, 4]}
+
+    Returns:
+        List[BoundingBox]: Ground truth bounding boxes
+    """
     ground_truth_list: List[BoundingBox] = []
     for idx in range(len(ground_truth_dict["labels"])):
         ground_truth_list.append(
@@ -46,6 +86,15 @@ def _generate_ground_truths(ground_truth_dict: Dict[str, Any]) -> List[BoundingB
 
 
 def _compute_iou(bbgt: Coordinates, bbdt: Coordinates) -> float:
+    """Compute IoU between detection and ground truth bounding boxes
+
+    Args:
+        bbgt (Coordinates): Coordinates of ground truth bounding box
+        bbdt (Coordinates): Coordinates of detection bounding box
+
+    Returns:
+        float: IoU score
+    """
     intersection_coordinates = [
         max(bbdt[0], bbgt[0]),
         max(bbdt[1], bbgt[1]),
@@ -70,6 +119,22 @@ def _compute_tp_fp_for_detections(
     ground_truth_list: List[BoundingBox],
     iou_threshold: float,
 ) -> Tuple[List[float], List[float]]:
+    """Compute true positives and false positives
+
+    Args:
+        count_true_positives (Dict[ClassName, int]): Dict containing number of true positives for
+            each bounding box class
+        class_name (ClassName): Class name of bounding box to compute true and false positives with
+        detections (List[BoundingBox]): List of detection bounding boxes to be used for computing
+            true and false positives
+        ground_truth_list (List[BoundingBox]): List of ground truth bounding boxes to be used for computing
+            true and false positives
+        iou_threshold (float): IoU threshold to distinguish between true and false positives
+
+    Returns:
+        Tuple[List[float], List[float]]: Tuple containing list of true positive flags and false positive flags
+            for each detection
+    """
     count_true_positives[class_name] = 0
     tp = [0.0] * len(detections)
     fp = [0.0] * len(detections)
@@ -102,6 +167,11 @@ def _compute_tp_fp_for_detections(
 
 
 def _compute_cumsum(values: List[float]) -> None:
+    """Helper function to compute cumulative sum of list of values in memory
+
+    Args:
+        values (List[float]): List of values to compute cumulative sum over
+    """
     cumsum = 0.0
     for idx, val in enumerate(values):
         values[idx] += cumsum
