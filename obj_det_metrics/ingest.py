6901eb01f2562106dfcc893e851ab9083830d7d2
yxlee245 <the.force.of.life@gmail.com>
2021-12-12 14:46:03 +0800

    Revert "Create functions to read from ground truth txts"
    This reverts commit baaf569a636e669a6a603d2d4cdc78c5959d95de.

--- obj_det_metrics/ingest.py
+++ obj_det_metrics/ingest.py
@@ -2,10 +2,9 @@ import os
 
 from obj_det_metrics.utils import (
     _generate_empty_detections_dict,
-    _generate_empty_ground_truths_dict,
     _read_file_lines_to_list,
 )
-from obj_det_metrics.variables import Detections, GroundTruths
+from obj_det_metrics.variables import Detections
 
 
 def read_detections_from_all_txts(txt_dir: str) -> Detections:
@@ -26,21 +25,3 @@ def _read_detections_from_single_txt(filepath: str, detections_dict: Detections)
         detections_dict["boxes"].append(coordinates)
         detections_dict["labels"].append(class_name)
         detections_dict["scores"].append(conf_score)
-
-
-def read_ground_truths_from_all_txts(txt_dir: str) -> GroundTruths:
-    ground_truth_dict = _generate_empty_ground_truths_dict()
-    txt_filepaths = [os.path.join(txt_dir, filename) for filename in os.listdir(txt_dir)]
-    for filepath in txt_filepaths:
-        _read_ground_truths_from_single_txt(filepath, ground_truth_dict)
-    return ground_truth_dict
-
-
-def _read_ground_truths_from_single_txt(filepath: str, ground_truth_dict: GroundTruths):
-    lines = _read_file_lines_to_list(filepath)
-    for line in lines:
-        line_contents = line.split()
-        class_name = line_contents[0]
-        coordinates = [int(content) for content in line_contents[1:]]
-        ground_truth_dict["boxes"].append(coordinates)
-        ground_truth_dict["labels"].append(class_name)
