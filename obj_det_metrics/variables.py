a13c07fa30630d4a035597cd68ef1785d9faa80b
yxlee245 <the.force.of.life@gmail.com>
2021-12-12 14:46:14 +0800

    Revert "Modify type annotations and add docstrings"
    This reverts commit b1b368886e7d08fa350ad49d46afb4a3e8f3061e.

--- obj_det_metrics/variables.py
+++ obj_det_metrics/variables.py
@@ -12,60 +12,26 @@ class BoundingBox:
         conf_score: float = 1.0,
         matched: bool = False,
     ):
-        """Initialize class variables
-
-        Args:
-            class_name (ClassName): Class name of bounding box
-            coordinates (Coordinates): Coordinates of bounding box, in the form [xmin, ymin, xmax, ymax]
-            conf_score (float, optional): Confidence score of bounding box. Defaults to 1.0.
-            matched (bool, optional): Applies to ground truth bounding boxes only. Indicates if ground truth box is
-                matched with detection bounding box. Defaults to False.
-        """
         self._coordinates = coordinates
         self._class_name = class_name
         self._conf_score = conf_score
         self._matched = matched
 
     def set_matched(self, matched: bool):
-        """Setter method to set value of `self._matched`
-
-        Args:
-            matched (bool): [description]
-        """
         self._matched = matched
 
     @property
     def coordinates(self) -> Coordinates:
-        """Returns read-only version of bounding box coordinates
-
-        Returns:
-            Coordinates: Bounding box coordinates, in the form [xmin, ymin, xmax, ymax]
-        """
         return self._coordinates
 
     @property
     def class_name(self) -> ClassName:
-        """Returns read-only version of bounding box class name
-
-        Returns:
-            ClassName: Bounding box class name
-        """
         return self._class_name
 
     @property
     def conf_score(self) -> float:
-        """Returns read-only version of confidence score
-
-        Returns:
-            float: Confidence score
-        """
         return self._conf_score
 
     @property
     def matched(self) -> bool:
-        """Returns read-only version of matched flag
-
-        Returns:
-            bool: Flag that indicates if ground truth bounding box is matched with detection bounding box
-        """
         return self._matched
